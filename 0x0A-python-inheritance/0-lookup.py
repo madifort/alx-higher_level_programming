4d6d85769026b6b9fc7c60fd55c0628383fd401c
Fortune Maduwuike <preshmadu29@gmail.com>
2024-02-05 12:00:44 -0800

    task solved

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module with the method lookpu"""
+"""Module with the method lookup"""
 
 
 def lookup(obj):
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,6 @@
 
 class MyList(list):
     """Class with method print_sorted"""
-    pass
 
     def print_sorted(self):
         """Methot that sorted a list"""
